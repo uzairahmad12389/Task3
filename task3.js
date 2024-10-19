78698aa7a595b984772ecb340d3fdd1a68b2b408
Uzair Ahmad <125276051+uzairahmad12389@users.noreply.github.com>
2024-10-19 22:32:50 +0500

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,8 @@
+function showFunFact() {
+    const funFact = document.getElementById('fun-fact');
+    if (funFact.style.display === 'none') {
+      funFact.style.display = 'block';
+    } else {
+      funFact.style.display = 'none';
+    }
+  }
